81122ff6a05851ce77e4e3ff2bf4d47300684455
GokiSakaguchi <40749181+GokiSakaguchi@users.noreply.github.com>
2019-02-19 21:42:36 +0900

    Add CRUD functions

--- src/controllers/todo-list-todo.controller.ts
+++ src/controllers/todo-list-todo.controller.ts
@@ -1,15 +1,89 @@
-import {repository} from '@loopback/repository';
-import {TodoListRepository} from '../repositories';
-import {post, param, requestBody} from '@loopback/rest';
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  del,
+  get,
+  getWhereSchemaFor,
+  param,
+  patch,
+  post,
+  requestBody,
+} from '@loopback/rest';
 import {Todo} from '../models';
+import {TodoListRepository} from '../repositories';
 
 export class TodoListTodoController {
   constructor(
     @repository(TodoListRepository) protected todoListRepo: TodoListRepository,
   ) {}
 
-  @post('/todo-list/{id}/todos')
-  async create(@param.path.number('id') id: number, @requestBody() todo: Todo){
+  @post('/todo-list/{id}/todos', {
+    responses: {
+      '200': {
+        description: 'TodoList.Todo model instance',
+        content: {'application/json': {schema: {'x-ts-type': Todo}}},
+      },
+    },
+  })
+  async create(
+    @param.path.number('id') id: number,
+    @requestBody() todo: Todo
+    ): Promise<Todo> {
     return await this.todoListRepo.todos(id).create(todo);
+  }
+
+  @get('/todo-lists/{id}/todos', {
+    responses: {
+      '200': {
+        description: "Array of Todo's belonging to TodoList",
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: {'x-ts-type': Todo}},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.path.number('id') id: number,
+    @param.query.object('filter') filters?: Filter,
+  ): Promise<Todo[]>{
+    return await this.todoListRepo.todos(id).find(filter);
+  }
+
+  @patch('/todo-lists/{id}/todos', {
+    responses: {
+      '200': {
+        description: 'TodoList.Todo PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async patch(
+    @param.path.number('id') id: number,
+    @requestBody() todo: Partial<Todo>,
+    @param.query.object('where', getWhereSchemaFor(Todo)) where?: Where,
+  ): Promise<Count>{
+    return await this.todoListRepo.todos(id).patch(todo, where);
+  }
+
+  @del('/todo-lists/{id}/todos', {
+    responses: {
+      '200': {
+        description: 'TodoList.Todo DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async delete(
+    @param.path.number('id') id: number,
+    @param.query.object('where', getWhereSchemaFor(Todo)) where?: Where,
+  ): Promise<Count> {
+    return await this.todoListRepo.todos(id).delete(where);
   }
 }
